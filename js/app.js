4fb65409ab2d3bfa3ae911276ef72950e8402a88
NATHAN LAMONT SMITH <andinto@gmail.com>
2019-03-30 03:43:34 -0500

    added resume, linked it; prepped JS for case studies to be added later

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-// console.log("testing connection");
+// // console.log("testing: linked up");
+
+// // cached elements: 
+// const tryCatchExpand = document.getElementById('try-catch-expand');
+// const tryCatchCollapse1 = document.getElementById('try-catch-collapse-1');
+// const tryCatchCollapse2 = document.getElementById('try-catch-collapse-2');
+// const tryCatchInfo = document.getElementById('try-catch-more-info');
+
+
+
+// // event listeners: 
+// tryCatchExpand.addEventListener('click', ()=>{
+// 	tryCatchExpand.style.display = "none";
+// 	tryCatchCollapse1.style.display = "flex";
+// 	tryCatchCollapse2.style.display = "flex";
+// 	tryCatchInfo.style.display = "contents";
+// })
+
+// tryCatchCollapse1.addEventListener('click', ()=>{
+// 	tryCatchExpand.style.display = "flex";
+// 	tryCatchCollapse1.style.display = "none";
+// 	tryCatchCollapse2.style.display = "none";
+// 	tryCatchInfo.style.display = "none";
+// })
+
+// tryCatchCollapse2.addEventListener('click', ()=>{
+// 	tryCatchExpand.style.display = "flex";
+// 	tryCatchCollapse1.style.display = "none";
+// 	tryCatchCollapse2.style.display = "none";
+// 	tryCatchInfo.style.display = "none";
+// })
